1daa2451ceacdcf113bac4809e820d7539602e4a
Anders Roxell <anders.roxell@linaro.org>
2019-10-29 13:41:22 +0100

    lkft: test_submit_for_testing: rework to make it usable after running
    Rework so curl down the latest artifact page and set the correct *_URL's
    so we can submit a job to test our definitions.
    
    Change-Id: I691487abed94df978f395fac34f0266c4fbb5c4a
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- openembedded-lkft/test_submit_for_testing.sh
+++ openembedded-lkft/test_submit_for_testing.sh
@@ -18,32 +18,19 @@ virtualenv --python=$(which python2) .venv
 source .venv/bin/activate
 pip install Jinja2 requests urllib3 ruamel.yaml
 
-export BUILD_ID=346
-export BUILD_NUMBER=346
+get_artifact() {
+    local local_prefix=${1}
+    local local_postfix=${2}
+    local artifact=$(grep ${local_prefix} index.txt|grep ${local_postfix}|tail -1|sed "s|.*${local_prefix}|${local_prefix}|"|sed "s|${local_postfix}.*|${local_postfix}|")
+    echo ${artifact}
+}
+
+export OE_VERSION=sumo
+export LINUX_TREE=linux-mainline
+export BUILD_NUMBER=latest
 export BASE_URL=http://snapshots.linaro.org
-export PUB_DEST=openembedded/lkft/morty/hikey/rpb/linux-mainline/${BUILD_NUMBER}
-export BOOOT_IMG=boot-Image-hikey-20171012090440-346.uefi.img
-export KERNEL_IMG=Image-gz-hikey-20171012090440-346.bin
-export MODULES_TGX=modules--hikey-20171012090440-346.tgz
-export DTB_IMG=Image-gz-hikey-20171012090440-346.dtb
-export ROOTFS_IMG=rpb-console-image-hikey-20171012090440-346.rootfs.img.gz
-export ROOTFS_EXT4=rpb-console-image-hikey-20171012090440-346.rootfs.ext4.gz
-export ROOTFS_TARXZ_IMG=rpb-console-image-hikey-20171012090440-346.rootfs.tar.xz
-export BOOT_URL=${BASE_URL}/${PUB_DEST}/${BOOT_IMG}
-export DTB_URL=${BASE_URL}/${PUB_DEST}/${DTB_IMG}
-export KERNEL_URL=${BASE_URL}/${PUB_DEST}/${KERNEL_IMG}
-export MODULES_URL=${BASE_URL}/${PUB_DEST}/${MODULES_TGZ}
-export EXT4_IMAGE_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_EXT4}
-export NFSROOTFS_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_TARXZ_IMG}
-export SYSTEM_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_IMG}
-export BUILD_URL="https://ci.linaro.org/job/openembedded-lkft-linux-mainline/DISTRO=rpb,MACHINE=hikey,label=docker-stretch-amd64/346/"
-export JOB_BASE_NAME="DISTRO=rpb,MACHINE=hikey,label=docker-stretch-amd64"
-export JOB_NAME="openembedded-lkft-linux-mainline/DISTRO=rpb,MACHINE=hikey,label=docker-stretch-amd64"
-export JOB_URL="https://ci.linaro.org/job/openembedded-lkft-linux-mainline/DISTRO=rpb,MACHINE=hikey,label=docker-stretch-amd64/"
 export KERNEL_BRANCH=master
 export KERNEL_COMMIT=ff5abbe799e29099695cb8b5b2f198dd8b8bdf26
-export KERNEL_CONFIG_URL=${BASE_URL}/${PUB_DEST}/config
-export KERNEL_DEFCONFIG_URL=${BASE_URL}/${PUB_DEST}/defconfig
 export KERNEL_DESCRIBE=v4.14-rc4-84-gff5abbe799e2
 export KERNEL_RECIPE=linux-hikey-mainline
 export KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
@@ -62,13 +49,11 @@ export LIBHUGETLBFS_VERSION=2.20
 export LTP_REVISION=e671f2a13c695bbd87f7dfec2954ca7e3c43f377
 export LTP_URL=git://github.com/linux-test-project/ltp.git
 export LTP_VERSION=20170929
-export MACHINE=hikey
 export MAKE_KERNELVERSION=4.14.0-rc4
 export MANIFEST_BRANCH=morty
 export QA_REPORTS_TOKEN=qa-reports-token
 export QA_SERVER=https://qa-reports.linaro.org
 export QA_SERVER_PROJECT=linux-mainline-oe
-export RECOVERY_IMAGE_URL=${BASE_URL}/${PUB_DEST}/juno-oe-uboot.zip
 export SKIP_LAVA=
 export SRCREV_kernel=ff5abbe799e29099695cb8b5b2f198dd8b8bdf26
 export BUILD_NAME="openembedded-lkft-linux-mainline"
@@ -83,11 +68,62 @@ export TOOLCHAIN="arm-linaro-linux-gnueabi linaro-6.2"
 
 export DRY_RUN=true
 
-for device in hi6220-hikey i386 x86 juno-r2 x15 dragonboard-410c; do
+for device in hi6220-hikey i386 x86 juno-r2 x15 dragonboard-410c qemu_arm qemu_arm64 qemu_i386 qemu_x86_64; do
     export DEVICE_TYPE=$device
+    case ${device} in
+        x15)
+            MACHINE=am57xx-evm
+            ;;
+        dragonboard-410c)
+            MACHINE=dragonboard-410c
+            ;;
+        hi6220-hikey)
+            MACHINE=hikey
+            ;;
+        i386)
+            MACHINE=intel-core2-32
+            ;;
+        x86)
+            MACHINE=intel-corei7-64
+            ;;
+        juno-r2)
+            MACHINE=juno
+            ;;
+        qemu_arm)
+            MACHINE=am57xx-evm
+            ;;
+        qemu_arm64)
+            MACHINE=hikey
+            ;;
+        qemu_i386)
+            MACHINE=intel-core2-32
+            ;;
+        qemu_x86_64)
+            MACHINE=intel-corei7-64
+            ;;
+    esac
+    export PUB_DEST=openembedded/lkft/lkft/${OE_VERSION}/${MACHINE}/lkft/${LINUX_TREE}/${BUILD_NUMBER}
+    curl -sL "${BASE_URL}/${PUB_DEST}" -o index.txt
+    export BOOT_URL="${BASE_URL}/${PUB_DEST}/$(get_artifact "boot-" ".uefi.img")"
+    export KERNEL_URL="${BASE_URL}/${PUB_DEST}/$(get_artifact "Image-" ".bin")"
+    export MODULES_URL="${BASE_URL}/${PUB_DEST}/$(get_artifact "modules-" ".tgz")"
+    export DTB_URL="${BASE_URL}/${PUB_DEST}/$(get_artifact "Image-" ".dtb")"
+    export SYSTEM_URL="${BASE_URL}/${PUB_DEST}/$(get_artifact "rpb-console-image-" ".rootfs.img.gz")"
+    export EXT4_IMAGE_URL="${BASE_URL}/${PUB_DEST}/$(get_artifact "rpb-console-image-" ".rootfs.ext4.gz")"
+    export NFSROOTFS_URL="${BASE_URL}/${PUB_DEST}/$(get_artifact "rpb-console-image-" ".rootfs.tar.xz")"
+    export RECOVERY_IMAGE_URL=${BASE_URL}/${PUB_DEST}/juno-oe-uboot.zip
+
+    export BUILD_URL="https://ci.linaro.org/job/openembedded-lkft-${LINUX_TREE}/DISTRO=rpb,MACHINE=${MACHINE},label=docker-stretch-amd64/NOT_CORRECT/"
+    export JOB_BASE_NAME="DISTRO=rpb,MACHINE=${MACHINE},label=docker-stretch-amd64"
+    export JOB_NAME="openembedded-lkft-linux-mainline/DISTRO=rpb,MACHINE=${MACHINE},label=docker-stretch-amd64"
+    export JOB_URL="https://ci.linaro.org/job/openembedded-lkft-${LINUX_TREE}/DISTRO=rpb,MACHINE=${MACHINE},label=docker-stretch-amd64/"
+    export KERNEL_CONFIG_URL=${BASE_URL}/${PUB_DEST}/config
+    export KERNEL_DEFCONFIG_URL=${BASE_URL}/${PUB_DEST}/defconfig
+    mv index.txt "${MACHINE}"-rootfs.index.txt
     bash submit_for_testing.sh
 done
 
 # cleanup virtualenv
 deactivate
 rm -rf .venv
+## vim: set sw=4 sts=4 et foldmethod=syntax : ##
